5f7f4a8842e016bcc543c6f983379f5fbc522241
Garima gupta <gaurigupta0008@gmail.com>
2025-03-13 17:13:35 +0530

    Add files via upload

---
+++ serializers.py
@@ -0,0 +1,13 @@
+from  .models import Moviedata
+from rest_framework import serializers
+
+
+
+
+class MovieSerializer(serializers.ModelSerializer):
+        image = serializers.ImageField(max_length=None, use_url = True)
+        class Meta:
+                model = Moviedata
+        fields = ['id','title','year','duration','rating','genre','image']
+
+        
